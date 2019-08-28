b790dd8c717f385ee9fc805d62962ffcd74054a3
no0neIO <37016546+no0neIO@users.noreply.github.com>
2019-08-28 16:01:05 +0300

    obstacles moving & game over

--- jsgame/app.js
+++ jsgame/app.js
@@ -2,6 +2,7 @@ var myGamePiece;
 
 function startGame() {
   myGamePiece = new component(30, 30, "red", 10, 120);
+  myObstacle = new component(10, 200, "green", 300, 120);
   myGameArea.start();
 }
 
@@ -12,10 +13,14 @@ var myGameArea = {
     this.canvas.height = 270;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.frameNo = 0;
     this.interval = setInterval(updateGameArea, 20);
   },
   clear: function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  stop: function() {
+    clearInterval(this.interval);
   }
 };
 
@@ -24,22 +29,80 @@ function component(width, height, color, x, y) {
   this.height = height;
   this.x = x;
   this.y = y;
+  this.speedX = 0;
+  this.speedY = 0;
   this.update = function() {
     ctx = myGameArea.context;
     ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
   };
+  this.newPos = function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  };
+  this.crashWith = function(otherobj) {
+    var myleft = this.x;
+    var myright = this.x + this.width;
+    var mytop = this.y;
+    var mybottom = this.y + this.height;
+    var otherleft = otherobj.x;
+    var otherright = otherobj.x + otherobj.width;
+    var othertop = otherobj.y;
+    var otherbottom = otherobj.y + otherobj.height;
+    var crash = true;
+    if (
+      mybottom < othertop ||
+      mytop > otherbottom ||
+      myright < otherleft ||
+      myleft > otherright
+    ) {
+      crash = false;
+    }
+    return crash;
+  };
 }
 
+var myObstacles = [];
+
 function updateGameArea() {
+  var x, y;
+  for (i = 0; i < myObstacles.length; i += 1) {
+    if (myGamePiece.crashWith(myObstacles[i])) {
+      myGameArea.stop();
+      return;
+    }
+  }
   myGameArea.clear();
+  myGameArea.frameNo += 1;
+  if (myGameArea.frameNo == 1 || everyInterval(150)) {
+    x = myGameArea.canvas.width;
+    y = myGameArea.canvas.height - 200;
+    myObstacles.push(new component(10, 200, "green", x, y));
+  }
+  for (i = 0; i < myObstacles.length; i += 1) {
+    myObstacles[i].x += -1;
+    myObstacles[i].update();
+  }
+  myGamePiece.newPos();
   myGamePiece.update();
 }
 
 document.addEventListener("keydown", logKey);
 function logKey(e) {
-  if (e.code == "KeyW" || e.code == "ArrowUp") myGamePiece.y -= 5;
-  if (e.code == "KeyS" || e.code == "ArrowDown") myGamePiece.y += 5;
-  if (e.code == "KeyA" || e.code == "ArrowLeft") myGamePiece.x -= 5;
-  if (e.code == "KeyD" || e.code == "ArrowRight") myGamePiece.x += 5;
+  if (e.code == "KeyW" || e.code == "ArrowUp") myGamePiece.speedY -= 1;
+  if (e.code == "KeyS" || e.code == "ArrowDown") myGamePiece.speedY += 1;
+  if (e.code == "KeyA" || e.code == "ArrowLeft") myGamePiece.speedX -= 1;
+  if (e.code == "KeyD" || e.code == "ArrowRight") myGamePiece.speedX += 1;
+}
+
+function stopMove() {
+  myGamePiece.speedX = 0;
+  myGamePiece.speedY = 0;
+}
+
+function everyInterval(n) {
+  if ((myGameArea.frameNo / n) % 1 == 0) {
+    return true;
+  }
+  return false;
 }
