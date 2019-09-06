4b5d072f66876bb785aa624aa2a833376b9257c7
no0neIO <37016546+no0neIO@users.noreply.github.com>
2019-09-06 18:01:59 +0300

    Pad Sound Test | some styling

--- mpcimulator/app.js
+++ mpcimulator/app.js
@@ -15,7 +15,17 @@ addPads = () => {
     pad.id = "pad" + i;
     pad.className = "pads";
     pad.innerText = `PAD - ${i}`;
-    document.getElementsByClassName("grid-16")[0].appendChild(pad);
+    pad[i] = document.getElementsByClassName("grid-16")[0].appendChild(pad);
+    document.getElementById("pad" + i).addEventListener("click", ps);
     console.log("DONE");
+    console.log(pad.id);
   }
+};
+
+ps = () => {
+  var audio = new Audio(
+    "https://freesound.org/data/previews/410/410514_5121236-lq.mp3"
+  );
+  audio.play();
+  console.log("BEEP");
 };
