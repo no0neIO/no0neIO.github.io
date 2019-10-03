500b6f5bd6cef07fd6c68c328111f04439dc7a37
no0neIO <37016546+no0neIO@users.noreply.github.com>
2019-10-03 12:48:59 +0300

    taking thumbnail src 150x150
    taking thumbnail src 150x150

---
+++ instagramphotos/app3.js
@@ -0,0 +1,63 @@
+document.getElementById("btn").addEventListener("click", getPhoto);
+
+function getPhoto() {
+  let url = document.getElementById("url").value;
+  url += "?__a=1";
+  let i = 0;
+  fetch(url)
+    .then(resp => resp.json())
+    .then(function(data) {
+      let profilePicHD = data.graphql.user.profile_pic_url_hd;
+      let res = data.graphql.user.edge_owner_to_timeline_media.edges;
+      res.forEach(element => {
+        console.log(element.node.thumbnail_resources[0].src);
+        document.getElementById(`gd${i}`).src =
+          element.node.thumbnail_resources[0].src;
+        let imgLink = element.node.shortcode;
+        imgLink = "https://www.instagram.com/p/" + imgLink;
+        document.getElementById(`l${i}`).href = imgLink;
+        i++;
+      });
+      console.log(res);
+      document.getElementById("dl").href = profilePicHD;
+      document.getElementById("profImg").src = profilePicHD;
+    });
+  document.getElementById("url").value = "";
+}
+
+// Lightbox Modal
+
+const lightbox = document.createElement("div");
+lightbox.id = "lightbox";
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll(".grid12 img");
+images.forEach(image => {
+  image.addEventListener("click", e => {
+    lightbox.classList.add("active");
+    const img = document.createElement("img");
+    img.src = image.src;
+    lightbox.appendChild(img);
+  });
+});
+
+lightbox.addEventListener("click", e => {
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove("active");
+  lightbox.innerHTML = "";
+});
+
+//
+// function getPhoto() {
+//   let url = document.getElementById("url").value;
+//   url += "?__a=1";
+//   fetch(url)
+//     .then(resp => resp.json())
+//     .then(function(data) {
+//       let test = data.graphql.hashtag.profile_pic_url;
+//       console.log(test);
+//       document.getElementById("dl").innerText = "DOWNLOAD NOW!";
+//       document.getElementById("dl").href = test;
+//     });
+//   document.getElementById("url").value = "";
+// }
