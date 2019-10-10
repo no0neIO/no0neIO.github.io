2867605780d0d265c2a99c576c9d487723ec4069
no0neIO <37016546+no0neIO@users.noreply.github.com>
2019-10-10 17:24:23 +0300

    new front probably

---
+++ testDesign/app.js
@@ -0,0 +1,11 @@
+// document.getElementsByClassName("item-0")
+
+x = document.getElementById("desk-lamp");
+
+function hover(element) {
+  x.setAttribute("src", "desk2.png");
+}
+
+function unhover(element) {
+  x.setAttribute("src", "desk.png");
+}
