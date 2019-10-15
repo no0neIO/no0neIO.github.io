7d8a74345314f8b5717598d6d13c38d9d59d3e5f
no0neIO <37016546+no0neIO@users.noreply.github.com>
2019-10-15 17:25:07 +0300

    soon gonna be main

--- testDesign/app.js
+++ testDesign/app.js
@@ -2,6 +2,8 @@
 
 x = document.getElementById("desk-lamp");
 
+// x.addEventListen("click", () => (document.body.style.background = "red"));
+
 function hover(element) {
   x.setAttribute("src", "desk2.png");
 }
@@ -9,3 +11,23 @@ function hover(element) {
 function unhover(element) {
   x.setAttribute("src", "desk.png");
 }
+
+// TEST
+
+const lines = [
+  "Code",
+  "Projects",
+  "Experience",
+  "Fun",
+  "Me",
+  "Music",
+  "Philoshopy"
+];
+
+changeLine = () => {
+  document.getElementById("customtext").innerText =
+    lines[Math.floor(Math.random() * 8)];
+  setTimeout(changeLine, 2000);
+};
+
+changeLine();
