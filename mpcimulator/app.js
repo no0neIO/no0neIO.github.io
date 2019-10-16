6f1b7970f60a376c72ae9a580556c950dda873c4
no0neIO <37016546+no0neIO@users.noreply.github.com>
2019-10-16 15:41:24 +0300

    new sounds/smaller size

--- mpcimulator/app.js
+++ mpcimulator/app.js
@@ -1,7 +1,20 @@
-sounds = ['sounds/Kick2.wav', 'sounds/Perc2.wav', 'sounds/Snare1.wav', 'sounds/Kick3.wav',
-  'sounds/Brash_Shaker.wav', 'sounds/Brutal_Hi_Hat.wav', 'sounds/Mafio_Snare.wav', 'sounds/kick.wav',
-  'sounds/boom.wav', 'sounds/snare.wav', 'sounds/hihat.wav', 'sounds/clap.wav',
-  'sounds/tom.wav', 'sounds/tink.wav', 'sounds/openhat.wav', 'sounds/ride.wav'
+sounds = [
+  "sounds/Hiphoparty samples/Bass Drum  Hip-Hop2-02.wav",
+  "sounds/Hiphoparty samples/BD1-02.wav",
+  "sounds/Hiphoparty samples/Snare Drum  Hip-Hop1-01.wav",
+  "sounds/Kick3.wav",
+  "sounds/Brash_Shaker.wav",
+  "sounds/Brutal_Hi_Hat.wav",
+  "sounds/Mafio_Snare.wav",
+  "sounds/kick.wav",
+  "sounds/boom.wav",
+  "sounds/snare.wav",
+  "sounds/hihat.wav",
+  "sounds/clap.wav",
+  "sounds/tom.wav",
+  "sounds/tink.wav",
+  "sounds/openhat.wav",
+  "sounds/ride.wav"
 ];
 
 function mpc() {
@@ -28,7 +41,7 @@ addPads = () => {
   }
 };
 
-ps = (j) => {
+ps = j => {
   const str = j.srcElement.id;
   console.log(str);
   for (let i = 0; i < 16; i++) {
@@ -45,20 +58,26 @@ ps = (j) => {
 };
 
 pinkPads = document.getElementById("pink");
-pinkPads.addEventListener("click", ch=(e)=>{
-  x=document.getElementsByClassName("pads");
-  x.className=''; 
-  for(i=0;i<16;i++){
-    x[i].classList.remove('padsWhatever');
-    x[i].classList.add('padsPink');
-  }
-})
+pinkPads.addEventListener(
+  "click",
+  (ch = e => {
+    x = document.getElementsByClassName("pads");
+    x.className = "";
+    for (i = 0; i < 16; i++) {
+      x[i].classList.remove("padsWhatever");
+      x[i].classList.add("padsPink");
+    }
+  })
+);
 
 pinkPads = document.getElementById("whatever");
-pinkPads.addEventListener("click", ch=(e)=>{
-  x=document.getElementsByClassName("pads");
-  for(i=0;i<16;i++){ 
-    x[i].classList.remove('padsPink');
-    x[i].classList.add('padsWhatever');
-  }
-})
+pinkPads.addEventListener(
+  "click",
+  (ch = e => {
+    x = document.getElementsByClassName("pads");
+    for (i = 0; i < 16; i++) {
+      x[i].classList.remove("padsPink");
+      x[i].classList.add("padsWhatever");
+    }
+  })
+);
