2b3d7f5214caf3a2ac1f15521276e6224ae4c30c
no0neIO <37016546+no0neIO@users.noreply.github.com>
2019-10-17 16:49:21 +0300

    added stylings

--- testDesign/app.js
+++ testDesign/app.js
@@ -21,7 +21,8 @@ const lines = [
   "Fun",
   "Me",
   "Music",
-  "Philoshopy"
+  "Philoshopy",
+  "Science"
 ];
 
 changeLine = () => {
@@ -31,3 +32,26 @@ changeLine = () => {
 };
 
 changeLine();
+
+//
+//
+// When the user scrolls the page, execute myFunction
+
+window.onscroll = function() {
+  myFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("wall");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky + 100) {
+    navbar.style.width = "30%";
+  } else if (window.pageYOffset < sticky + 150) {
+    navbar.style.width = "0%";
+  }
+}
