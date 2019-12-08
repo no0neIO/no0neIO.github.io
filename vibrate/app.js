3684c6c654add5f814c8fa5fbd05576a226f6500
no0neIO <37016546+no0neIO@users.noreply.github.com>
2019-12-08 15:14:55 +0200

    Delete app.js

--- vibrate/app.js
+++
@@ -1,4 +0,0 @@
-console.log('HELLO');
-
-window.navigator.vibrate(200); // vibrate for 200ms
-window.navigator.vibrate([100, 30, 100, 30, 100, 30, 200, 30, 200, 30, 200, 30, 100, 30, 100, 30, 100]);
