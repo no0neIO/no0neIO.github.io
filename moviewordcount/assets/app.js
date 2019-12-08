73e90791c10f4fc4922aa6ae681138c211d9bf1a
no0neIO <37016546+no0neIO@users.noreply.github.com>
2019-12-08 23:44:01 +0200

    script stats

--- moviewordcount/assets/app.js
+++ moviewordcount/assets/app.js
@@ -2,6 +2,9 @@ const fn = document.getElementById('fileName')
 const searchstat = document.getElementById('searchStat')
 const keyword = document.getElementById('keyword')
 const fileContent = document.getElementById('fileContent')
+const docLenght = document.getElementById('docLenght')
+const wordCount = document.getElementById('wordCount')
+const charCount = document.getElementById('charCount')
 
 function loadBook(filename, displayName) {
     let currentBook = ""
@@ -21,12 +24,28 @@ function loadBook(filename, displayName) {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             currentBook = xhr.responseText;
-
+            c = currentBook
             // regex for formatting text
             currentBook = currentBook.replace(/(?:\r\n|\r|\n)/g, '<br>')
 
             fileContent.innerHTML = currentBook
             fileContent.scrollTop = 0
+            getStats(c)
         }
+    }
+}
+
+function getStats(currentBook) {
+    const text = currentBook.toLowerCase()
+    const wordArray = text.match(/\b\S+\b/g)
+    console.log(wordArray)
+    const wordDictionary = {}
+    wordCount.innerHTML = `Word Count: ${wordArray.length}`
+    charCount.innerHTML = `Character Count: ${text.replace(/\s+/g, '').length}`;
+
+    // count every word in wordArray
+    for (let word in wordArray) {
+        let wordValue = wordArray[word]
+            (wordDictionary[wordValue]) ? wordDictionary[wordValue]++ : wordDictionary[wordValue] = 1
     }
 }
