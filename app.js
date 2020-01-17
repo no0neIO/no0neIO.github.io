1fbe583e99b625c5ab3889461d95b60a70d24c63
no0neIO <37016546+no0neIO@users.noreply.github.com>
2020-01-17 16:13:44 +0200

    added tvs project

--- app.js
+++ app.js
@@ -32,26 +32,3 @@ changeLine = () => {
 };
 
 changeLine();
-
-//
-//
-// When the user scrolls the page, execute myFunction
-
-// window.onscroll = function() {
-//   myFunction();
-// };
-
-// Get the navbar
-// var navbar = document.getElementById("wall");
-
-// Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset >= sticky + 500) {
-//     navbar.style.width = "30%";
-//   } else if (window.pageYOffset < sticky + 150) {
-//     navbar.style.width = "0%";
-//   }
-// }
