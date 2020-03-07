ab31190bebc2b89e12a0246633db99c738560c84
no0neIO <37016546+no0neIO@users.noreply.github.com>
2020-03-07 12:45:19 +0200

    ~last version

--- smartwave/js/app.js
+++ smartwave/js/app.js
@@ -1,49 +1,50 @@
-let typingTimer = Function;                // timer identifier function for debounce
-let doneTypingInterval = 250;
+let typingTimer = Function // timer identifier function for debounce
+let doneTypingInterval = 250 // time in ms
 
-const searchText = $('.search-input');
-let resultsList = $('.results');
+const searchText = $('.search-input')
+let resultsList = $('.results')
 
+// after keyup start timer.
 searchText.keyup(function () {
-    clearTimeout(typingTimer);
-
-    let inputLen = searchText.val().length;
-    if (inputLen > 1) { // an to length tou searh term einai > 1
-        typingTimer = setTimeout(doneTyping, doneTypingInterval); // teleiwse pithanws to typing, kalese tin done typing
-    } else { //if (!inputLen) { // an to text den exei contents na svinei to results div
-        resultsList.empty();
+    clearTimeout(typingTimer)
+    let inputLen = searchText.val().length
+    if (inputLen > 1) { // if lenght of search term > 1
+        typingTimer = setTimeout(doneTyping, doneTypingInterval); //user probably finished typing, call doneTyping function
+    } else {
+        resultsList.empty()
     }
 });
 
-//user is "finished typing," do something
+//get request
 function doneTyping() {
     resultsList.empty(); // katharise ta proigoumena results
-    let keywords = searchText.val();
+    let keywords = searchText.val()
     const url = 'https://onelineof.me/smartwave/customers.json';
     resultsList.show();
     $.ajax({
         type: 'GET',
         url: url,
-        success: data => successFunc(data, keywords),
+        success: data => successFunc(data, keywords), // if successful call successFunc
         statusCode: {
             400: function () {
                 alert('Errors in specified keywords and/or language.');
             }
         },
         error: function (err) {
-            console.log(err);
+            console.log(err)
         }
     });
 }
 
 
 function successFunc(data, keywords) {
-    keywords = keywords.toUpperCase()
-    $.each(data.customers, (index, item) => {
+    keywords = keywords.toUpperCase() // making uppercase the user input and the name from the JSON for case insensitive check
+    $.each(data.customers, (index, item) => { // for every name on the JSON we check for matches
         let name = item.contactName.toUpperCase()
-        let firstName = name.split(' ').slice(0, -1).join(' ');
-        let lastName = name.split(' ').slice(-1).join(' ');
-        if (firstName.startsWith(keywords) || lastName.startsWith(keywords))
+        name = name.split(' ')  // spliting the full name from the JSON 
+        let firstName = name[0] //so we can search for both first and last name
+        let lastName = name[1]
+        if (firstName.startsWith(keywords) || lastName.startsWith(keywords)) //if we find a match we append a new div in results class
             resultsList.append(`
             <div class="result">
                 <div class="result-name">${item.contactName}</div>
